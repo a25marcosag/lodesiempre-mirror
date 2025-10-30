47190abfefa85acce9fe0d1eb1d9d55d2b057c6b
a25marcosag <a25marcosag@iessanclemente.net>
2025-10-30 18:35:35 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,12 @@
-test
+#!/bin/bash
+set -e
+
+mkdir -p /app/database
+touch /app/database/database.sqlite
+chown -R www-data:www-data /app
+
+php artisan config:clear
+php artisan cache:clear
+php artisan migrate --force
+
+php artisan serve --host=0.0.0.0 --port=10000
