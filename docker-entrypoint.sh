8f420851d15233fa63808f750315684d6bbb28b2
a25marcosag <a25marcosag@iessanclemente.net>
2025-12-09 16:41:15 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p /app/database
-touch /app/database/database.sqlite
 chown -R www-data:www-data /app
-
-sed -i "s/DB_CONNECTION=.*/DB_CONNECTION=sqlite/g" /app/.env
-sed -i "s/DB_DATABASE=.*/DB_DATABASE=\/app\/database\/database.sqlite/g" /app/.env
 
 php artisan config:clear
 php artisan cache:clear
@@ -14,4 +9,4 @@ php artisan route:clear
 php artisan view:clear
 php artisan migrate:fresh --force --seed
 
-php artisan serve --host=0.0.0.0 --port=10000
+exec apache2-foreground
